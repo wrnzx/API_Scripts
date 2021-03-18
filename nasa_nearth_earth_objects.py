dc5448b21c72242ea83e517f021638d68b2c830a
Warren <80871212+wrnzx@users.noreply.github.com>
2021-03-17 21:10:29 -0700

    Update nasa_nearth_earth_objects.py

--- nasa_nearth_earth_objects.py
+++ nasa_nearth_earth_objects.py
@@ -1,45 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 import math
 from datetime import date, timedelta
-# This script will only print 1 object for each day of the week
+# this script will only return one object for each day (can be changed with a loop to return all object per day)
 
 
-def get_response():  # Function get and returns JSON response
-    # API Data
-    api_key = ' '
-    starting_date = date.today()
+def get_response():  # function to get and returns JSON response from NASA
 
-    api_url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + starting_date.strftime(
+    # API data
+    api_key = ' '  # enter your API Key here. Available from: https://api.nasa.gov/
+
+    api_url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + date.today().strftime(
         '%Y-%m-%d') + '&api_key=' + api_key
-    # API Request
+
+    # API request
     response = requests.get(api_url).json()
-    return response  
+    return response
 
 
 data = get_response()  # returns API Response in JSON format
-start_date = date.today()  # Today's date to pass into path
+date = date.today()  # today's date to pass into path
 
-for each_entry in data['near_earth_objects']:  # Iterate through list of near earth objects
+for neo in data['near_earth_objects']:  # iterate through list of near earth objects
 
-    near_earth_object = data['near_earth_objects'][str(start_date)][0]  # Path to first [0] object on specified day
+    near_earth_object = data['near_earth_objects'][str(date)][0]  # Location of first near earth object on specified day
 
-    # Naming and hazard info
+    # naming and hazard info
     neo_id = near_earth_object['id']
     neo_name = near_earth_object['name']
     jpl_url = near_earth_object['nasa_jpl_url']
 
-    # Convert hazard bool to yes or no
+    # convert hazard bool to yes or no
     if near_earth_object['is_potentially_hazardous_asteroid']:
         potential_hazard = 'Yes'
     else:
         potential_hazard = 'No'
 
-    # Estimated diameter and approach date
+    # estimated diameter and approach date
     estimated_diameter = round(near_earth_object['estimated_diameter']['feet']['estimated_diameter_max'])
     estimated_approach_date = near_earth_object['close_approach_data'][0]['close_approach_date_full']
     relative_velocity = float(near_earth_object['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'])
 
-    # Print out data
+    # print out data
     print('')
     print('_____________________________________________________________DATA_BY_NASA_API_____')
     print('')
@@ -52,5 +55,5 @@ for each_entry in data['near_earth_objects']:  # Iterate through list of near ea
     print('NASA Jet Propulsion Lab Url:', jpl_url)
     print('__________________________________________________________________________________')
 
-    start_date = start_date + timedelta(days=1)
+    date = date + timedelta(days=1)  # advance date by 1 to print next day in week
 
